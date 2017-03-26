242c01f7e6886fcb080a91e840119b44af3fd358
Celmor <Celmor@users.noreply.github.com>
2017-03-26 06:55:16 +0200

    description update

--- qemu-networking.sh
+++ qemu-networking.sh
@@ -6,17 +6,17 @@ sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE #eth1: internet facing
 sudo iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -s 10.0.0.0/16 -i br0 -m conntrack --ctstate NEW -j ACCEPT
 sudo bash -c "iptables-save > /etc/iptables/iptables.rules"
-
-# setup (/etc/libvirt/hooks/daemon if using libvirt or top of qemu start script):
+ 
+# setup (put this in/etc/libvirt/hooks/daemon if using libvirt or top of qemu start script):
 pidfile=qemu_setup.pid
 if [ ! -f "/tmp/$pidfile" ]; then
-	ip link add name br0 type bridge
-	ip addr add 10.0.1.1/24 dev br0 # <- this is your gateway
-	ip link set br0 up
-	ip tuntap add dev tap0 mode tap
-	ip link set tap0 master br0
-	ip link set tap0 up promisc on
-	echo $$ > $pidfile
-	fi
+    ip link add name br0 type bridge
+    ip addr add 10.0.1.1/24 dev br0 # <- this is your gateway
+    ip link set br0 up
+    ip tuntap add dev tap0 mode tap
+    ip link set tap0 master br0
+    ip link set tap0 up promisc on
+    echo $$ > $pidfile
+    fi
 # rest of qemu start script if not using libvirt:
 qemu-system-x86_64 -net nic,model=e1000 -net tap,ifname=tap0,script=no,downscript=no # ... other options ...
