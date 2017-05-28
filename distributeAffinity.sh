a70c04a11915c3bd72e5433eb0d618f8602fc324
Celmor <Celmor@users.noreply.github.com>
2017-05-29 01:01:22 +0200

    Update distributeAffinity.sh

--- distributeAffinity.sh
+++ distributeAffinity.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-i=0; n=8
+i=0; n=6; offset=2
 pidstat -thu -G "$1" 1 1 | tail -n +5 | sort -nrk 8,8 | awk '{ print $4 }' | while read pid ; do
-taskset -cp $((i % n)) $pid; (( i++ )); done >/dev/null
+taskset -cp $((i % n + 2)) $pid; (( i++ )); done >/dev/null
