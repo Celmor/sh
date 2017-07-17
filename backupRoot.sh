4204616dc10ca7ea27e6e836249b19a8b9ec7feb
Celmor <Celmor@users.noreply.github.com>
2017-07-17 03:24:48 +0200

    Update backupRoot.sh

--- backupRoot.sh
+++ backupRoot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+[ "$1" == "-h" ] && printf '%s\n' "Usage: BackupDir=BACKUPDIR $0 [-h] [--zfs]" && exit
 [ $EUID == 0 ] || { echo "This script must be run as root"; exit 0; }
 date="$(date +d%F-%0H-%0M)"
-BackupDir="/Data/Backup/Linux/currentRoot"
+BackupDir=${BackupDir:-"/DataBackup/Backup/Linux/currentRoot"}
 BackupMount="$(df -P "$BackupDir" | awk 'END { print $NF }')"
 [ "$1" == "--zfs" ] && BackupDS="$(df -P "$BackupDir" | awk 'END { print $1 }')"
 exclude="/{dev,proc,sys,tmp,run,mnt,media,lost+found,Data,home,usr/share/locale,var,srv,root,$BackupMount}/\*"
 
-rsync -ahxHAX --checksum --info=progress2 --delete --exclude="$exclude" --delete-excluded --delete / "$BackupDir"/ && \
+rsync -ahxHAX --checksum --info=progress2 --delete --exclude="$exclude" --delete-excluded / "$BackupDir"/ && \
 [ "$1" == "--zfs" ] && sudo zfs snapshot "$BackupDS"@"$date" && echo "$BackupDS@$date created"
