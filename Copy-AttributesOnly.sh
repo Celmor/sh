d5ac4958609bc7e338f76c7009388728e35d85a4
Celmor <Celmor@users.noreply.github.com>
2017-09-06 23:57:15 +0200

    Update Copy-AttributesOnly.sh

--- Copy-AttributesOnly.sh
+++ Copy-AttributesOnly.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # alternative for `cp --attributes-only`
 # https://askubuntu.com/questions/56792/how-to-copy-only-file-attributes-metadata-without-actual-content-of-the-file#56810
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 REFERENCE TARGET"
+	exit 1
+fi
 chmod --reference="$1" "$2"
 chown --reference="$1" "$2"
 touch --reference="$1" "$2"
