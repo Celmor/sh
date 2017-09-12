61374fc3191a084784552767cf613d037bbcb588
Celmor <Celmor@users.noreply.github.com>
2017-09-12 10:38:56 +0200

    Update and rename zfs-compare.sh to zfs-func

--- zfs-compare.sh
+++
@@ -1,5 +0,0 @@
-# to source in bashrc
-pool=( Data DataBackup DataNew )
-getPool(){ [ $# -ne 1 ] && exit 1; [ "$(dirname "$1")" = "/" ] && printf '%s\n' "$1" && exit 0; printf '%s\n' "$(getPool "$(dirname "$1")")"; }
-comparePool(){ root="$(getPool $1)"; root=${root///}; for i in ${pool[@]}; do [ "$root" = "$i" ] && continue; printf '%s\n' "comparing "$root" -> $i:"; sudo rsync -axHAXS --delete -i --dry-run "$1/" /"$i${1//"/$root"}"/; done; }
-syncPool(){ root="$(getPool $1)"; root=${root///}; for i in ${pool[@]}; do [ "$root" = "$i" ] && continue; printf '%s\n' "comparing "$root" -> $i:"; sudo rsync -axHAXS -i --delete "$1/" /"$i${1//"/$root"}"/; done; }
