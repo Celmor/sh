b04009d8b42f9d8546800b0a275c6895b6f07c5f
Celmor <Celmor@users.noreply.github.com>
2017-10-12 01:25:22 +0200

    adds checks; cleanup

--- Insert-Line.sh
+++ Insert-Line.sh
@@ -2,10 +2,14 @@
 # inserts TEXT into FILE at LINE
 [ $# -ne 3 ] && printf '%s\n' "Usage: $0 \"FILE\" LINE \"TEXT\"" && exit 1
 file="$1"
+[ -f "$file" ] || printf '%s\n' "Error: \"$file\" is not a regular file" && exit 1
 line="$2"
+[ "$line" -lt 0 ] || printf '%s\n' "Error: \"$line\" is invalid line number" && exit 1
 text="$3"
+[ -n "$text" ] || printf '%s\n' "Error: No input text" && exit 1
 output=""
-i=0
+i=0 # count line number from this value up
+
 # read through file
 while read -r out ; do
         # add all lines before insert position to buffer
@@ -18,5 +22,6 @@ while read -r out ; do
 	output+="$out"'\n'
 done < <(printf '%s\n' "$file")
 # have read through file and filled buffer
+
 # now flush buffer to file
 printf %s > "$file"
