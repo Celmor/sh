556c70907026cee50fb59f523b40a8c39473e432
Celmor <Celmor@users.noreply.github.com>
2017-12-13 00:51:07 +0100

    Update autotype.sh

--- autotype.sh
+++ autotype.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # types stdin out
 # useful where copying via clipboard doesn't work
-sleep "$1"s
+
+sleep "${1-2}"s
 while read -r line; do
-	xdotool type "$line"
+	xdotool type --delay 50 "$line"
 	xdotool key Return
 done
