545dee3ff381eb56d182e214bcef41efe852efe5
Celmor <celmor@mail.com>
2018-01-06 15:10:15 +0100

    adds checks; adds checksum comparison

--- checkFS.sh
+++ checkFS.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+
 fs="$1"
 if ! mountpoint -q "$fs"; then
-	printf "ERROR: %s isn't a mountpoint\\n"
-	exit 1
+    printf "ERROR: \""%s\"" isn't a mountpoint\\n"
+    exit -1
 fi
 file="$(mktemp -tp "$fs" checkFS.XXX)"
-avail="$(($(
-	df --sync -k --output=avail "$fs" | tail -1
-	)/1024-1))M"
-trap 'rm "$file"' SIGINT
-pv -petrabSs "$avail" /dev/urandom \
-	| tee "$fs"/rand \
-	| md5sum
-md5sum "$file"
+availM=$(($(df --sync -k --output=avail "$fs" | tail -1)/1024-1))
+if [ "$availM" -le 0 ]; then
+    printf "ERROR: Error: Not enough free space on %s\\n" "$fs"
+    exit -1
+fi
+
+trap 'rm "$file"' SIGINT EXIT
+Sum="$(pv -petrabSs "${availM}M" /dev/urandom \
+    | tee "$file" \
+    | md5sum \
+    | awk '{ print $1}'
+)"
+if [ "$Sum" != "$(md5sum "$file" | awk '{ print $1}')" ]; then
+    printf %s\\n "Verification failed!"
+    exit 1
+fi
+#md5sum --quiet -c <(printf "%s  %s\\n" "$Sum" "$file")
