2b10ad77a4328134938ab84e6195c8e89b7bbf5a
Celmor <celmor@mail.com>
2018-03-21 14:55:32 +0100

    update/fix, simplified

--- mpvCam.sh
+++ mpvCam.sh
@@ -3,8 +3,7 @@ cam=$1
 url=$2
 if [[ $# -eq 2 && "$cam" =~ ^[0-9]+$ && ! "$url" =~ ( |\') ]]; then
     echo "Receiving URL"...
-    mapfile -t temp < <(youtube-dl -g --playlist-items $cam $url)
-    mpv "${temp[0]}" --audio-file="${temp[1]}" 2>&1 | grep -v libva
+    mpv "$(youtube-dl -g --playlist-items $cam $url)"
 else
     echo "Wrong input: $0 <Cam> <URL>"
 fi
