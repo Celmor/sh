ea6c2dd7b453cf3174d50de8410e626f9491832e
Celmor <celmor@mail.com>
2018-05-10 18:01:57 +0200

    working version, add description (v1.0)

--- deduplicate.sh
+++ deduplicate.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+# deduplicate files in current path by searching for files with matching checksums (md5) and hardlinking them
+
 declare -A hashtable
 while read -r line; do
     hash="${line%% *}"
     # empty file
     if [ "$hash" = "d41d8cd98f00b204e9800998ecf8427e" ]; then
-		continue
-	# fill
+        continue
+    # fill
     elif [ "${hashtable["$hash"]}" = "" ]; then
         hashtable["$hash"]=${line#*  }
-	# collision
+    # collision
     else
-        printf '\n%s\n%s\n ' "${line#*  }" "${hashtable["$hash"]}"
+        ln -f "${hashtable["$hash"]}" "${line#*  }"
     fi
 done < <(find . -type f -exec md5sum {} +)
